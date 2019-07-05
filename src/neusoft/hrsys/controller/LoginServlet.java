c88b7e21003f36ba28e26006d973778135487ca4
Jss <529103458@qq.com>
2019-07-05 19:53:03 +0800

    first

--- src/neusoft/hrsys/controller/LoginServlet.java
+++ src/neusoft/hrsys/controller/LoginServlet.java
@@ -18,7 +18,7 @@ import neusoft.hrsys.entity.HrAdmin;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
+    /** 123
      * @see HttpServlet#HttpServlet()
      */
     public LoginServlet() {
